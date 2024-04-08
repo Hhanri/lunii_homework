0df4c3afaee5e15b86166f06648c94ef2f9f7851
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-08 12:41:56 +0200

    [search_movies] movie preview entity

---
+++ lib/features/search_movies/domain/entities/movie_preview_entity.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+import 'package:lunii_homework/core/enums/movie_type.dart';
+import 'package:lunii_homework/core/value_objects/url.dart';
+
+abstract base class MoviePreviewEntity extends Equatable {
+
+  final String id;
+  final String title;
+  final int year;
+  final Url posterUrl;
+  final MovieType type;
+
+  const MoviePreviewEntity({
+    required this.id,
+    required this.title,
+    required this.year,
+    required this.posterUrl,
+    required this.type
+  });
+
+}
