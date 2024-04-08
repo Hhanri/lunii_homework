9dd293df037deb3cb583cc3fa496f503ed00ce80
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-08 12:13:30 +0200

    [core] app padding

---
+++ lib/core/theme/app_padding.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/widgets.dart';
+
+final class AppPadding {
+
+  AppPadding._();
+
+  static const all4 = EdgeInsets.all(4);
+  static const all8 = EdgeInsets.all(4);
+  static const all16 = EdgeInsets.all(4);
+
+  static const h4 = EdgeInsets.symmetric(horizontal: 4);
+  static const h8 = EdgeInsets.symmetric(horizontal: 8);
+  static const h16 = EdgeInsets.symmetric(horizontal: 16);
+
+  static const v4 = EdgeInsets.symmetric(vertical: 4);
+  static const v8 = EdgeInsets.symmetric(vertical: 8);
+  static const v16 = EdgeInsets.symmetric(vertical: 16);
+
+}
