ede1e820eeecc843eedfe38fcd71dd3f7e362650
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-08 20:10:06 +0200

    [movie_info] movie info entity placeholder

--- lib/features/movie_info/domain/entities/movie_info_entity.dart
+++ lib/features/movie_info/domain/entities/movie_info_entity.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:lunii_homework/core/enums/movie_type.dart';
 import 'package:lunii_homework/core/value_objects/url.dart';
+import 'package:lunii_homework/features/movie_info/data/models/movie_info_model.dart';
 
 abstract base class MovieInfoEntity extends Equatable {
 
@@ -36,5 +37,21 @@ abstract base class MovieInfoEntity extends Equatable {
     required this.runtime,
   });
 
+  factory MovieInfoEntity.placeholder() => MovieInfoModel(
+    id: '0000',
+    title: '████████████',
+    rated: '??',
+    released: DateTime(1970, 1, 1),
+    language: '████',
+    genres: const [],
+    director: '████ ████',
+    writers: const [],
+    actors: const [],
+    plot: "",
+    posterUrl: null,
+    rating: 0.0,
+    type: MovieType.movie,
+    runtime: const Duration(minutes: 0)
+  );
 
 }
