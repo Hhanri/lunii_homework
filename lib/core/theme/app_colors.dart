6a7c793961aeb735352f5e7da7f7245e0f96f44c
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-08 16:59:22 +0200

    [core] app colors background and separator

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -11,4 +11,7 @@ final class AppColors {
   static const errorHighlight = Color(0xfffe665b);
   static const errorBackground = Color(0xffe5c9c7);
 
+  static const background = Color(0xfff9fafa);
+
+  static const separator = Color(0xffeef0f2);
 }
