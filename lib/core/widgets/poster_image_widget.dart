c5c6e28871514c1094f714dbbb4b22c71cc410a2
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-08 16:39:59 +0200

    [core] poster image widget

---
+++ lib/core/widgets/poster_image_widget.dart
@@ -0,0 +1,45 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:lunii_homework/core/theme/app_colors.dart';
+import 'package:lunii_homework/core/value_objects/url.dart';
+
+class PosterImageWidget extends StatelessWidget {
+
+  final Url? url;
+  final double? size;
+
+  const PosterImageWidget({
+    super.key,
+    required this.url,
+    this.size
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size ?? 100,
+      width: size ?? 100,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.white,
+          width: 2
+        ),
+        borderRadius: BorderRadius.circular(4),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            spreadRadius: 2,
+            blurRadius: 14,
+            offset: Offset(0, 2)
+          )
+        ]
+      ),
+      child: url == null
+        ? const ColoredBox(color: AppColors.errorBackground)
+        : CachedNetworkImage(
+            imageUrl: url!.value,
+            fit: BoxFit.fill
+          )
+    );
+  }
+}
