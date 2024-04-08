caa16a695b21f7cb4aa581a4e05531312871ec54
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-08 16:59:41 +0200

    [core] app shaping

---
+++ lib/core/theme/app_shaping.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final class AppShaping {
+
+  AppShaping._();
+
+  static const radius8 = Radius.circular(8);
+  static const radius16 = Radius.circular(16);
+  static const radius24 = Radius.circular(24);
+
+  static const borderRadius8 = BorderRadius.all(radius8);
+  static const borderRadius16 = BorderRadius.all(radius16);
+  static const borderRadius24 = BorderRadius.all(radius24);
+
+}
