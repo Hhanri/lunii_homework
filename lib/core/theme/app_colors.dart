415ad1bd919a2950a0a418cbd03028de31f19709
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-08 12:06:27 +0200

    [core] app colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+final class AppColors {
+
+  AppColors._();
+
+  static const secondary = Color(0xfff2be50);
+  static const primary = Color(0xffa0d3eb);
+  static const primaryHighlight = Color(0xff3466e1);
+
+  static const errorHighlight = Color(0xfffe665b);
+  static const errorBackground = Color(0xffe5c9c7);
+
+}
